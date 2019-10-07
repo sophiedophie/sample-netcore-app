bf72cde9d0b8e03dd2fa24f73b51417e526f2780
sophiedophie <seohee.sophie.kwon@gmail.com>
2019-10-07 10:41:58 +0900

    Sample hello world .netcore app with circleci

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace netcoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
